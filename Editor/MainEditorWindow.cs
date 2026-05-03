2185771f80d644e14c047d5bdb79206586f17fa9
legotrainkid <62917897+legotrainkid@users.noreply.github.com>
2026-05-03 04:23:05 -0400

    Beginning Editor Shenanigans

---
+++ Editor/MainEditorWindow.cs
@@ -0,0 +1,139 @@
+﻿﻿using FUSE.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UI.Common;
+using UI.Builder;
+using GalaSoft.MvvmLight.Messaging;
+using Game.Events;
+using TMPro;
+using UnityEngine.EventSystems;
+
+namespace FUSE.Editor
+{
+    class MainEditorWindow : MonoBehaviour
+    {
+        private Window _window;
+        private UIPanel _panel;
+        private static MainEditorWindow _instance;
+        private bool _isMapLoaded = false;
+
+        public static MainEditorWindow Shared
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<MainEditorWindow>();
+                }
+                return _instance;
+            }
+        }
+
+        public static void Setup()
+        {
+
+        }
+
+        private void Awake()
+        {
+            _window = GetComponent<Window>();
+        }
+
+        private void OnEnable()
+        {
+            // Register for map events
+            Messenger.Default.Register<MapDidLoadEvent>(this, OnMapLoaded);
+            Messenger.Default.Register<MapWillUnloadEvent>(this, OnMapUnload);
+        }
+
+        private void OnDisable()
+        {
+            // Unregister from events
+            Messenger.Default.Unregister(this);
+
+            if (_panel != null)
+            {
+                _panel.Dispose();
+                _panel = null;
+            }
+        }
+
+        private void OnMapLoaded(MapDidLoadEvent _)
+        {
+            _isMapLoaded = true;
+            CreateWindow(); // Create window after map loads
+        }
+
+        private void OnMapUnload(MapWillUnloadEvent _)
+        {
+            _isMapLoaded = false;
+            if (_window != null && _window.IsShown)
+            {
+                _window.CloseWindow();
+            }
+        }
+
+        public static void Toggle()
+        {
+            // Only allow toggling if map is loaded
+            if (!Shared._isMapLoaded) return;
+
+            GameObject currentSelectedGameObject = EventSystem.current.currentSelectedGameObject;
+            bool flag = currentSelectedGameObject != null && currentSelectedGameObject.GetComponent<TMP_InputField>() != null;
+            if (flag) return;
+
+            if (Shared._window.IsShown)
+            {
+                Shared.Close();
+            }
+            else
+            {
+                Shared.Show();
+            }
+        }
+
+        public void Show()
+        {
+            if (!_isMapLoaded) return;
+            Populate();
+            _window.ShowWindow();
+        }
+
+        private void Populate()
+        {
+            _window.Title = "Node Editor";
+
+            if (_panel != null)
+            {
+                _panel.Dispose();
+            }
+
+            _panel = WindowCreatorHelper.Shared.PopulateWindow(_window, builder =>
+            {
+
+            });
+        }
+
+        public void CreateWindow()
+        {
+            if (!WindowCreatorHelper.CanCreateWindow)
+                return;
+
+            _window = WindowCreatorHelper.Shared.CreateWindow(400, 500, Window.Position.Center);
+            _window.Title = "FUSE Editor";
+        }
+
+        public void Close()
+        {
+            if (Shared._window.IsShown)
+            {
+                Shared._window.CloseWindow();
+            }
+
+        }
+    }
+}
---
+++ Infrastructure/WindowCreatorHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Builder;
+using UI.Common;
+using UI;
+using UnityEngine;
+
+namespace FUSE.Infrastructure
+{
+    public class WindowCreatorHelper
+    {
+        private static WindowCreatorHelper _instance;
+        public static WindowCreatorHelper Shared => _instance ?? (_instance = new WindowCreatorHelper());
+
+        public static bool CanCreateWindow =>
+            UnityEngine.Object.FindObjectOfType<ProgrammaticWindowCreator>(true) != null;
+
+        private static Window CreateWindowInternal(string identifier, int width, int height, Window.Position position)
+        {
+            var creator = UnityEngine.Object.FindObjectOfType<ProgrammaticWindowCreator>(true);
+            if (creator == null)
+                throw new NullReferenceException();
+
+            var window = creator.windowPrefab;
+            var instance = UnityEngine.Object.Instantiate(window, creator.transform, false);
+            instance.SetInitialPositionSize(identifier, new Vector2(width, height), position, Window.Sizing.Fixed(new Vector2Int(width, height)));
+            instance.name = identifier;
+
+            return instance;
+        }
+
+        private static UIPanel PopulateWindowInternal(Window window, Action<UIPanelBuilder> closure)
+        {
+            var creator = UnityEngine.Object.FindObjectOfType<ProgrammaticWindowCreator>(true);
+            if (creator == null)
+                throw new NullReferenceException();
+
+            return UIPanel.Create(window.contentRectTransform, creator.builderAssets, closure);
+        }
+
+        public Window CreateWindow(int width, int height, Window.Position position)
+        {
+            // Generate a unique identifier for the window
+            string identifier = "ligma" + DateTime.Now.Ticks;
+            return CreateWindowInternal(identifier, width, height, position);
+        }
+
+        public UIPanel PopulateWindow(Window window, Action<UIPanelBuilder> closure)
+        {
+            return PopulateWindowInternal(window, closure);
+        }
+    }
+}
+
